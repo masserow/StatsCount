63220f23560b4e1a5a9691305bf7e28f8b4d04a9
Sergey Masserow <masserow@mail.ru>
2023-02-26 18:21:05 +0300

    new test

--- src/test/java/ru/netology/statistic/StatsCount/service/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatsCount/service/StatisticsServiceTest.java
@@ -6,10 +6,22 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void shouldfindMax() {
+    void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void IfMaxNoFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
